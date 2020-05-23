bf55b4172339a1dced2c5c3930c9899a11988aaf
ilya07092000 <ischenko2012@gmail.com>
2020-05-23 20:52:51 +0300

    new js

--- script.js
+++ script.js
@@ -1,112 +1,124 @@
 let bird = document.querySelector('.bird'),
-    birdPos,
-    currentBirdPos,
-    mainImg = document.querySelector('.main__img'),
-    reloadBtn = document.querySelector('.reload__btn'),
-    intervalBird,
-    topBarrier,
-    bottomBarrier,
     barriers = document.querySelectorAll('.barriers'),
+    mainImg = document.querySelector('.main__img'),
     counterBlock = document.querySelector('.count'),
-
+    reloadBtn = document.querySelector('.reload__btn'),
+    instruction = document.querySelector('.instruction'),
+    birdStylePosTop,
+    jump = -10,
+    birdDown = 9,
+    barriersSpeed = 30,
+    intervalBird,
+    birdCurrentPos,
     counter = 0;
     counterBlock.innerHTML = `Score: ${counter}`;
 
-function birdDown() {
-    birdPos = getComputedStyle(bird);
-    currentBirdPos = bird.getBoundingClientRect();
-    let birdPosTop = parseInt(birdPos.top);
-    let newPos = String(birdPosTop + 10) + 'px';
-    bird.style.top = newPos;
+// СИЛА ПРИТЯЖЕНИЯ
+function birdFlyDown() {
+    birdStylePosTop = parseInt(getComputedStyle(bird).top);
+    bird.style.top = String(birdStylePosTop + birdDown) + 'px';
 
+    barrierMove();
+    barrierCreate();
+    score();
+    checkCollision();
+}
+
+// ПРЫЖОК 
+document.addEventListener('keydown', birdJump);
+document.addEventListener('click', birdJump);
+function birdJump() {
+    clearInterval(intervalBird)
+    bird.style.top = String(birdStylePosTop + jump) + 'px';
+    intervalBird = setInterval(birdFlyDown, 100);
+    instruction.style.display = 'none';
+}
+
+// ДВИЖЕНИЕ ПРЕПЯТСТВИЙ
+function barrierMove() {
+    barriers = document.querySelectorAll('.barriers');
+    for(let i = 0; i < barriers.length; i++) {
+        let barriersPos = getComputedStyle(barriers[i]);
+        barriersPos = new WebKitCSSMatrix(barriersPos.webkitTransform).m41 - barriersSpeed;;
+        barriers[i].style.transform = "translateX(" + `${barriersPos}` + 'px' +")";
+    }
+}
+
+// СОЗДАНИЕ НОВОГО ПРЕПЯТСТВИЯ
+function barrierCreate() {
+    let currentBarrier = barriers.length - 1;
+    let lastBarrier = getComputedStyle(barriers[currentBarrier]);
+    let lastBarrierPos = getComputedStyle(barriers[currentBarrier]);
+    lastBarrierPos = new WebKitCSSMatrix(lastBarrierPos.webkitTransform).m41;
+
+    if(lastBarrierPos < -180) {
+        let newBarrier = document.createElement('div');
+        newBarrier.classList.add('barriers')
+        newBarrier.innerHTML = `
+        <div class="barriers__inner">
+            <img class="top__barrier" src="img/flappy_bird_pipeUp.png" alt="">
+            <img class="bottom__barrier" src="img/flappy_bird_pipeBottom.png" alt="">
+        </div>
+        `
+        mainImg.append(newBarrier)
+        let newLastBarrier = mainImg.lastElementChild;
+        let barriersInner = newLastBarrier.lastElementChild;
+        topBarrier = barriersInner.firstElementChild;
+        let bottomBarrier = barriersInner.lastElementChild;
+
+        topBarrier.style.top = -Math.random() * 125 + 'px';
+        bottomBarrier.style.top = 242 + parseInt(topBarrier.style.top) + 60 + 'px';
+    }
+}
+
+// СЧЕТ
+function score() {
+    let barrierCurrentPos = barriers[counter].getBoundingClientRect();
+    birdCurrentPos = bird.getBoundingClientRect();
+
+    if(birdCurrentPos.left > barrierCurrentPos.left + barriers[counter].offsetWidth) {
+        counter++;
+        counterBlock.innerHTML = `Score: ${counter}`;
+    }
+
+}
+
+// ПРОВЕРКА НЕ ВРЕЗАЛАСЬ ЛИ ПТИЦА В ПРЕПЯТСТВИЕ
+function checkCollision() {
     let barierInner = barriers[counter].firstElementChild;
     let topB = barierInner.firstElementChild;
     let bottomB = barierInner.lastElementChild;
     let topBPos = topB.getBoundingClientRect();
     let bottomBPos = bottomB.getBoundingClientRect();
-    let gg = bird.getBoundingClientRect();
 
-    if(gg.top <= topBPos.top + 242 && gg.left >= topBPos.left && gg.left <= topBPos.left + topB.offsetWidth) {
+    if(birdCurrentPos.top <= topBPos.top + 242 
+        && birdCurrentPos.left >= topBPos.left 
+        && birdCurrentPos.left <= topBPos.left + topB.offsetWidth) {
         collision()
     }
-    if(gg.top <= topBPos.top + 242 && gg.left + bird.offsetWidth >= topBPos.left && gg.left + bird.offsetWidth <= topBPos.left + topB.offsetWidth) {
-        collision()
-    }
-
-    if(gg.top + bird.offsetHeight >= bottomBPos.top && gg.left >= topBPos.left && gg.left <= topBPos.left + topB.offsetWidth) {
-        collision()
-    }
-    if(gg.top + bird.offsetHeight >= bottomBPos.top && gg.left + bird.offsetWidth >= topBPos.left && gg.left + bird.offsetWidth <= topBPos.left + topB.offsetWidth) {
+    if(birdCurrentPos.top <= topBPos.top + 242 
+        && birdCurrentPos.left + bird.offsetWidth >= topBPos.left 
+        && birdCurrentPos.left + bird.offsetWidth <= topBPos.left + topB.offsetWidth) {
         collision()
     }
 
-
-    for(let i = 0; i < barriers.length; i++) {
-        barriers = document.querySelectorAll('.barriers');
-        let currentEl = barriers.length - 1;
-        let lastBarrier = getComputedStyle(barriers[currentEl]);
-        lastBarrier = new WebKitCSSMatrix(lastBarrier.webkitTransform);
-        lastBarrier = lastBarrier.m41;
-
-        let barriersPos = getComputedStyle(barriers[i]);
-        barriersPos = new WebKitCSSMatrix(barriersPos.webkitTransform);
-        barriersPos = barriersPos.m41
-        barriersPos -= 35;
-        barriers[i].style.transform = "translateX(" + `${barriersPos}` + 'px' +")";
-
-        let barrierCurrentPos = barriers[counter].getBoundingClientRect();
-        let birdCurrentPos = bird.getBoundingClientRect();
-        if(birdCurrentPos.left > barrierCurrentPos.left + barriers[counter].offsetWidth) {
-            counter++;
-            counterBlock.innerHTML = `Score: ${counter}`;
-        }
-
-        if(lastBarrier < -150) {
-            let newBarrier = document.createElement('div');
-            newBarrier.classList.add('barriers')
-            newBarrier.innerHTML = `
-            <div class="barriers__inner">
-                <img class="top__barrier" src="img/flappy_bird_pipeUp.png" alt="">
-                <img class="bottom__barrier" src="img/flappy_bird_pipeBottom.png" alt="">
-            </div>
-            `
-            mainImg.append(newBarrier)
-
-            let newLastBarrier = mainImg.lastChild;
-            let barriersInner = newLastBarrier.lastElementChild;
-                topBarrier = barriersInner.firstElementChild;
-                bottomBarrier = barriersInner.lastElementChild;
-            
-            let topBarrierStyleTop = -Math.random() * 150;
-            let bottomBarrierStyleBottom = 242 + topBarrierStyleTop + 60
-            topBarrier.style.top = topBarrierStyleTop + 'px';
-            bottomBarrier.style.top = bottomBarrierStyleBottom + 'px';
-        }
+    if(birdCurrentPos.top + bird.offsetHeight >= bottomBPos.top 
+        && birdCurrentPos.left >= topBPos.left 
+        && birdCurrentPos.left <= topBPos.left + topB.offsetWidth) {
+        collision()
+    }
+    if(birdCurrentPos.top + bird.offsetHeight >= bottomBPos.top 
+        && birdCurrentPos.left + bird.offsetWidth >= topBPos.left 
+        && birdCurrentPos.left + bird.offsetWidth <= topBPos.left + topB.offsetWidth) {
+        collision()
     }
 }
 
-
-document.addEventListener('keydown', jump)
-document.addEventListener('click', jump)
-function jump() {
-    clearInterval(intervalBird)
-    let newBirdPos = getComputedStyle(bird);
-    let newBirdPosTop = parseInt(newBirdPos.top);
-    let afterJump = String(newBirdPosTop - 15) + 'px'
-    bird.style.top = afterJump
-    intervalBird = setInterval(birdDown, 100)
-}
-
-reloadBtn.addEventListener('click', reload);
-function reload() {
-    location.reload();
-}
-
-
+// ПРОИГРЫШ
 function collision() {
     clearInterval(intervalBird)
-    document.removeEventListener('keydown', jump)
-    document.removeEventListener('click', jump)
+    document.removeEventListener('keydown', birdJump)
+    document.removeEventListener('click', birdJump)
     bird.style.zIndex = '999999999'
     bird.style.transform = 'rotate(90deg)'
     bird.style.transition = 'all 2s';
@@ -114,15 +126,8 @@ function collision() {
     reloadBtn.style.display = 'inline-block';
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
+// НАЧАТЬ ЗАНОВО
+reloadBtn.addEventListener('click', reload);
+function reload() {
+    location.reload();
+}
